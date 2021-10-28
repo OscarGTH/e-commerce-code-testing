10597cd88a643be18f73ae15b78d263972679460
Oskari Niskanen <oskari.niskanen97@gmail.com>
2021-10-28 23:23:49 +0300

    Removed jest-chain for now because of a bug. Finalized test cases for clamp.js

--- tests/clamp.test.js
+++ tests/clamp.test.js
@@ -1,5 +1,30 @@
+/*
+ * Unit tests for Clamp module (clamp.js)
+ */
+
 import clamp from '../src/clamp.js';
 
-test('returns clamped value', () => {
-  expect(clamp(1,2,3)).toEqual(2);
+test('returns clamped value with values 1,2,3', () => {
+  expect(clamp(1,2,3)).toBe(2);
 });
+
+test('returns clamped value with values 2,2,3', () => {
+  expect(clamp(2,2,3)).toBe(2);
+});
+
+test('returns clamped value with values 2,1,3', () => {
+  expect(clamp(2,1,3)).toBe(2);
+});
+
+test('returns clamped value with values 3,1,3', () => {
+  expect(clamp(3,1,3)).toBe(3);
+});
+
+test('returns clamped value with values 4,1,3', () => {
+  expect(clamp(4,1,3)).toBe(3);
+});
+
+test('returns clamped value with values 0.5,0.6,1.5', () => {
+  expect(clamp(0.5,0.6,1.5)).toBe(0.6);
+});
+
